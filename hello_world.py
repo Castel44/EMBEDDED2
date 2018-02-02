99c50714f7af52460a07d7dbc43773d211c024a0
Andrea <a.castellani93@gmail.com>
2018-02-02 17:13:45 +0100

    test

---
+++ hello_world.py
@@ -0,0 +1,45 @@
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
+for num in range(1,21):
+    string = ""
+    if num % 3 == 0:
+        string = string + "Fizz"
+    if num % 5 == 0:
+        string = string + "Buzz"
+    if num % 5 != 0 and num % 3 != 0:
+        string = string + str(num)
+    print(string)
+
+
+#LOGREG
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.linspace(-10, 10, 100)
+sig = 1 / (1 + np.exp(-t))
+plt.figure(figsize=(9, 3))
+plt.plot([-10, 10], [0, 0], "k-")
+plt.plot([-10, 10], [0.5, 0.5], "k:")
+plt.plot([-10, 10], [1, 1], "k:")
+plt.plot([0, 0], [-1.1, 1.1], "k-")
+plt.plot(t, sig, "b-", linewidth=2, label=r"$\sigma(t) = \frac{1}{1 + e^{-t}}$")
+plt.xlabel("t")
+plt.legend(loc="upper left", fontsize=20)
+plt.axis([-10, 10, -0.1, 1.1])
+plt.show()
