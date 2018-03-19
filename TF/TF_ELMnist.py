9c101d1fc65f05be2528c280affc7039727a2dcd
Andrea <a.castellani93@gmail.com>
2018-03-19 23:33:31 +0100

    Made function and hypar

--- TF/TF_ELMnist.py
+++ TF/TF_ELMnist.py
@@ -1,12 +1,79 @@
 import os
 import numpy as np
 import tensorflow as tf
-import sys
-from keras.datasets import mnist
+import time
+import itertools
 import keras
+from keras.datasets import mnist
+
+
+# TODO: fix correct histogram and variable TensorBoard
+def ELM_classificator(size_in, size_out, n_neuron, batch_size, norm):
+    tf.reset_default_graph()
+    sess = tf.Session()
+
+    with tf.name_scope('inputs'):
+        x = tf.placeholder(tf.float32, shape=[None, size_in * size_in], name="x")
+        y = tf.placeholder(tf.float32, shape=[None, size_out], name="labels")
+
+    with tf.name_scope('hidden_layer'):
+        w = tf.Variable(tf.random_normal(shape=[size_in * size_in, n_neuron], stddev=1), trainable=False)
+        b = tf.Variable(tf.random_normal(shape=[n_neuron], stddev=1), trainable=False)
+        H = tf.sigmoid(tf.matmul(x, w) + b)  # H idden reprs
+        tf.summary.histogram("weights", w)
+        tf.summary.histogram("biases", b)
+        tf.summary.histogram("activations", H)
+
+    with tf.name_scope('train'):
+        I = tf.eye(n_neuron, dtype=tf.float32)
+        HH = tf.Variable(tf.add(tf.zeros(n_neuron), tf.div(I, 10 ** norm)), name='HH')
+        HT = tf.Variable(tf.zeros([n_neuron, size_out]), name='HT')
+        HH_op = tf.assign(HH, tf.add(HH, tf.matmul(H, H, transpose_a=True)))
+        HT_op = tf.assign(HT, tf.add(HT, tf.matmul(H, y, transpose_a=True)))
+        B = tf.matmul(tf.matrix_inverse(HH), HT)
+        tf.summary.histogram("B", B)
+        tf.summary.histogram("HH", HH)
+        tf.summary.histogram("HT", HT)
+
+    with tf.name_scope('output_layer'):
+        y_proba = tf.matmul(H, B)
+
+    with tf.name_scope("accuracy"):
+        correct_prediction = tf.equal(tf.argmax(y_proba, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar("accuracy", accuracy)
+
+    summ = tf.summary.merge_all()
+    sess.run(tf.global_variables_initializer())
+
+    hparm = 'n_%d,norm_%d' % (n_neuron, norm)
+    writer = tf.summary.FileWriter(LOGDIR + hparm)
+    writer.add_graph(sess.graph)
+
+    print("Starting run for %s" % hparm)
+    nb = int(np.ceil(x_train.shape[0] / batch_size))
+    t0 = time.time()
+    for i in range(nb):
+        print('Processing batch %d/%d' % (i + 1, nb))
+        x_batch = x_train[i * batch_size:((i + 1) * batch_size)].astype('float32')
+        y_batch = y_train[i * batch_size:((i + 1) * batch_size)].astype('float32')
+        [_, _, s] = sess.run([HH_op, HT_op, summ], feed_dict={x: x_batch, y: y_batch})
+        writer.add_summary(s, i)
+
+    print('Training done in: ', time.time() - t0)
+    acc_train = accuracy.eval(feed_dict={x: x_train, y: y_train}, session=sess)
+    print('Train accuracy: ', acc_train)
+    acc_test = accuracy.eval(feed_dict={x: x_test, y: y_test}, session=sess)
+    print('Test accuracy: ', acc_test)
+    print('#' * 100)
+    return acc_train, acc_test
+
+
+######################################################################################################################
 
 LOGDIR = '/tmp/TF_ELM_mnist/'
 
+print('MNIST DATASET')
 mnist_data = mnist.load_data()
 train_set, test_set = mnist_data
 x_train, y_train = train_set
@@ -20,70 +87,24 @@ y_test = y_test.astype('float32')
 
 x_train = x_train.reshape(-1, 28 * 28)
 x_test = x_test.reshape(-1, 28 * 28)
-print(x_train.shape)
+print('x_train shape: ', x_train.shape)
+# Dataset not normalized
 
-n_neuron = 2048
-norm = 10 ** 6
+# HYPEPARAMETERS
 batch_size = 5000
+neuron_number = (1024, 2048)
+norm = (4, 0, -4)
 
-tf.reset_default_graph()
+train_acc = []
+test_acc = []
+run = 0
+run_num = list(itertools.product(neuron_number, norm)).__len__()
+for v in itertools.product(neuron_number, norm):
+    print('Starting run %d/%d' % (run + 1, run_num))
+    train, test = ELM_classificator(28, 10, v[0], batch_size, v[1])
+    train_acc.append(train)
+    test_acc.append(test)
+    run += 1
 
-with tf.name_scope('inputs'):
-    x = tf.placeholder(tf.float32, shape=[None, 28 * 28], name="x")
-    y = tf.placeholder(tf.float32, shape=[None, 10], name="labels")
-
-with tf.name_scope('hidd'):
-    w = tf.Variable(tf.random_normal(shape=[28 * 28, n_neuron], stddev=1), trainable=False)
-    b = tf.Variable(tf.random_normal(shape=[n_neuron], stddev=1), trainable=False)
-    H = tf.sigmoid(tf.matmul(x, w) + b)  # H idden reprs
-    tf.summary.histogram("weights", w)
-    tf.summary.histogram("biases", b)
-    tf.summary.histogram("activations", H)
-
-with tf.name_scope('train'):
-    I = tf.eye(n_neuron, dtype=tf.float32)
-    HH = tf.Variable(tf.add(tf.zeros(n_neuron), tf.div(I, norm)), name='HH')
-    HT = tf.Variable(tf.zeros([n_neuron, 10]), name='HT')
-    HH_op = tf.assign(HH, tf.add(HH, tf.matmul(H, H, transpose_a=True)))
-    HT_op = tf.assign(HT, tf.add(HT, tf.matmul(H, y, transpose_a=True)))
-    # Does need assign for B?
-    B = tf.matmul(tf.matrix_inverse(HH), HT)
-    tf.summary.histogram("B", B)
-    tf.summary.histogram("HH", HH)
-    tf.summary.histogram("HT", HT)
-
-with tf.name_scope('output_layer'):
-    y_proba = tf.matmul(H, B)
-
-with tf.name_scope("accuracy"):
-    correct_prediction = tf.equal(tf.argmax(y_proba, 1), tf.argmax(y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    tf.summary.scalar("accuracy", accuracy)
-
-
-with tf.Session() as sess:
-    tf.global_variables_initializer().run()
-    summ = tf.summary.merge_all()
-    writer = tf.summary.FileWriter(LOGDIR)
-    writer.add_graph(sess.graph)
-
-    print("initialized")
-    # TODO; better, ceil
-    nb = x_train.shape[0] // batch_size
-
-    for i in range(nb):
-        print('Processing batch %d/%d' % (i + 1, nb))
-        x_batch = x_train[i * batch_size:((i + 1) * batch_size)].astype('float32')
-        y_batch = y_train[i * batch_size:((i + 1) * batch_size)].astype('float32')
-        [_, _, s] = sess.run([HH_op, HT_op, summ], feed_dict={x: x_batch, y: y_batch})
-        writer.add_summary(s, i)
-
-    print('B: ', B.eval())
-    acc_train = accuracy.eval(feed_dict={x: x_train, y: y_train})
-    print('\nB: ', B.eval())
-    acc_test = accuracy.eval(feed_dict={x: x_test, y: y_test})
-
-print('Train accuracy: ', acc_train)
-print('Test accuracy: ', acc_test)
 
 os.system('tensorboard --logdir=%s' % LOGDIR)
