5ebd942652b9698977bbebef9988078fe4b10398
Andrea <a.castellani93@gmail.com>
2018-03-23 14:55:10 +0100

    Fixed graph

--- TF/elm.py
+++ TF/elm.py
@@ -13,19 +13,19 @@ class elm(object):
             output_size,
             n_neurons,
             savedir,
-            activation= tf.sigmoid,
-            type = 'c',
-            name = "",
-            w_initializer = 'default',
-            b_initializer = 'default',
-            l2norm=0.,
-
+            activation=tf.sigmoid,
+            type='c',
+            name="",
+            w_initializer='default',
+            b_initializer='default',
+            l2norm=None,
+            batch_size=1000
     ):
 
         self.input_size = input_size
         self.output_size = output_size
         self.n_neurons = n_neurons
-        self.l2norm = l2norm
+        self.batch_size = batch_size
         self.w_initializer = w_initializer
         self.b_initializer = b_initializer
         self.savedir = savedir
@@ -36,64 +36,54 @@ class elm(object):
         self.HH = None
         self.HT = None
 
+        if l2norm is None:
+            # TODO: do with tf precision
+            l2norm = 50 * np.finfo(np.float32).eps
+        self.l2norm = l2norm
 
         # start tensorflow session
         self.sess = tf.Session()
-
+        # Build TensorFlow graph
         # define graph inputs
         with tf.name_scope("input"):
-
-            self.x = tf.placeholder(dtype='float32', shape=[None, input_size])
-            self.y = tf.placeholder(dtype='float32', shape=[None, output_size])
+            self.x = tf.placeholder(dtype='float32', shape=[None, input_size], name='x')
+            self.y = tf.placeholder(dtype='float32', shape=[None, output_size], name='labels')
 
         with tf.name_scope("hidden_layer"):
-
             if self.w_initializer is 'default' and self.b_initializer is 'default':
-
-
-                init_w = tf.random_normal(shape=[self.input_size, self.n_neurons],
-
-                                  stddev=tf.sqrt(tf.div(2.,
-                                                        tf.add(tf.cast(self.input_size, 'float32'),
-                                                        tf.cast(self.output_size, 'float32')))))
-
-                self.Hw = tf.Variable(init_w)
-
-
-                init_b = tf.random_normal(shape=[self.n_neurons],
-
-                                          stddev=tf.sqrt(tf.div(2.,
-                                                                tf.add(tf.cast(self.input_size, 'float32'),
-                                                                tf.cast(self.output_size, 'float32')))))
-
-                self.Hb = tf.Variable(init_b)
-
-
+                init_w = tf.random_normal_initializer(stddev=tf.div(3.0, tf.sqrt(tf.cast(input_size, tf.float32))))
+                init_b = tf.random_normal_initializer()
             else:
-
                 print("Using custom inizialization for ELM: %s" % self.name)
+                init_w = w_initializer
+                init_b = b_initializer
 
-                self.Hw = w_initializer
+                assert self.w_initializer or self.b_initializer is not 'default', "Both w_initializer and b_initializer " \
+                                                                                  "should be provided when using custom initialization"
 
-                self.Hb = b_initializer
-
-                assert self.Hw or self.Hb is not 'default', "Both w_initializer and b_initializer " \
-                                                           "should be provided when using custom initialization"
-
-
-                assert self.Hw.shape.as_list() is not [input_size, n_neurons], "Invalid shape for hidden layer weights tensor"
-                assert self.Hb.shape.as_list() is not [n_neurons], "Invalid shape for hidden layer biases tensor"
-
+            self.Hw = tf.get_variable('Hw', shape=[input_size, n_neurons], dtype=tf.float32, initializer=init_w,
+                                      trainable=False)
+            self.Hb = tf.get_variable('Hb', shape=[n_neurons], dtype=tf.float32, initializer=init_b, trainable=False)
+            tf.get_variable_scope().reuse_variables()
 
             self.H = self.activation(tf.matmul(self.x, self.Hw) + self.Hb)
 
+        with tf.name_scope('output_layer'):
+            self.B = tf.Variable(tf.zeros(shape=[n_neurons, output_size]), dtype=tf.float32, name='B')
+            self.y_out = tf.matmul(self.H, self.B)
 
-            with tf.name_scope('output_layer'):
-
-                    self.B = tf.Variable(tf.zeros(shape=[self.n_neurons, self.output_size]), dtype='float32')
-
-                    self.y_out = tf.matmul(self.H, self.B)
-
+        if self.type is 'c':
+            with tf.name_scope("accuracy"):
+                self.correct_prediction = tf.equal(tf.argmax(self.y_out, 1), tf.argmax(self.y, 1))
+                self.metric = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+        elif self.type is 'r':  # regression
+            with tf.name_scope("mean_squared_error"):
+                # nb possible numerical instability
+                # https://stackoverflow.com/questions/41338509/tensorflow-mean-squared-error-loss-function
+                # TODO
+                self.metric = tf.reduce_mean(tf.squared_difference(self.y_out, self.y, name='mse'))
+        else:
+            raise ValueError("Invalid argument for type")
 
         # initialization
         self.sess.run([self.Hw.initializer, self.Hb.initializer])
@@ -102,183 +92,96 @@ class elm(object):
         self.writer = tf.summary.FileWriter(self.savedir + self.name)
         self.writer.add_graph(self.sess.graph)
 
-
-
-
-    def evaluate(self, dataset, batch_size=10000):
-
+    def evaluate(self, dataset):
         # create tensorflow iterator
-        batched_dataset = dataset.batch(batch_size=batch_size)
-
-        # create tensorflow iterator
+        batched_dataset = dataset.batch(batch_size=self.batch_size)
         iterator = batched_dataset.make_initializable_iterator()
-
         next_batch = iterator.get_next()
-
         self.sess.run(iterator.initializer)
-
         metric_vect = []
-
         while True:
             try:
                 x_batch, y_batch = self.sess.run(next_batch)
-
-                metric_vect.append(self.sess.run(self.metric,feed_dict={self.x: x_batch, self.y: y_batch}))
-
+                metric_vect.append(self.sess.run(self.metric, feed_dict={self.x: x_batch, self.y: y_batch}))
             except tf.errors.OutOfRangeError:
-
                 break
+        return np.mean(
+            metric_vect)  # TODO self.y_out return ? maybe predic retuns only y_out and then another method computer accuracy
 
-        return np.mean(metric_vect) #TODO self.y_out return ? maybe predic retuns only y_out and then another method computer accuracy
-
-
-
-    def train(self, dataset, batch_size = 2000):
-
+    def train(self, dataset):
         # define training structure
-
         with tf.name_scope("training"):
-
             # initialization and training graph definition
-
-            self.HH = tf.Variable(tf.multiply(tf.eye(self.n_neurons, dtype=tf.float32),
-                                              tf.cast(self.l2norm, tf.float32)),
-                                              name='HH')
-
+            self.HH = tf.Variable(tf.multiply(tf.eye(self.n_neurons, dtype=tf.float32), self.l2norm), name='HH')
             self.HT = tf.Variable(tf.zeros([self.n_neurons, self.output_size]), name='HT')
 
-
             train_op = tf.group(
-             tf.assign_add(self.HH, tf.matmul(self.H, self.H, transpose_a=True)),
-             tf.assign_add(self.HT, tf.matmul(self.H, self.y, transpose_a=True))
+                tf.assign_add(self.HH, tf.matmul(self.H, self.H, transpose_a=True)),
+                tf.assign_add(self.HT, tf.matmul(self.H, self.y, transpose_a=True))
             )
-
-
+            # TODO: check for correct matrix inversion HH
             B_op = tf.assign(self.B, tf.matmul(tf.matrix_inverse(self.HH), self.HT))
 
-
-
-        if self.type is 'c':
-
-            # no need for cost function # TODO
-
-            with tf.name_scope("accuracy"):
-                correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(self.y_out, 1))
-                self.metric = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-        elif self.type is 'r': # regression
-            with tf.name_scope("mean_squared_error"):
-                # nb possible numerical instability
-                # https://stackoverflow.com/questions/41338509/tensorflow-mean-squared-error-loss-function
-                # TODO
-
-                self.metric = tf.reduce_mean(tf.squared_difference(self.y_out,self.y, name='mse'))
-
-        else:
-
-            raise ValueError("Invalid argument for type")
-
         # add to graph
-
         self.writer.add_graph(self.sess.graph)
 
         # Initialize a saver to store model checkpoints
-        saver = tf.train.Saver()
-
-        batched_dataset = dataset.batch(batch_size=batch_size)
+        # saver = tf.train.Saver()
 
         # create tensorflow iterator
+        batched_dataset = dataset.batch(batch_size=self.batch_size)
         iterator = batched_dataset.make_initializable_iterator()
-
         next_batch = iterator.get_next()
 
-        nb = int(np.ceil(dataset._tensors[1]._shape_as_list()[0]/batch_size)) # ceil ?? #TODO
+        nb = int(np.ceil(dataset._tensors[1]._shape_as_list()[0] / self.batch_size))  # ceil ?? #TODO
 
         # initialize variables
-
         self.sess.run([self.HH.initializer, self.HT.initializer])
         self.sess.run(iterator.initializer)
 
         t0 = time.time()
-
         print("{} Start training...".format(datetime.now()))
-        print("{} Open Tensorboard at --logdir {}".format(datetime.now(),
-                                                        self.savedir))
+        print("{} Open tensorboard at --logdir={}".format(datetime.now(), self.savedir))
 
-        batch=1
+        batch = 1
         while True:
             try:
-
                 start = time.time()
-
                 # get next batch of data
                 x_batch, y_batch = self.sess.run(next_batch)
-
                 # Run the training op
-                self.sess.run(train_op, feed_dict={self.x: x_batch,
-                                                   self.y: y_batch})
-
+                self.sess.run(train_op, feed_dict={self.x: x_batch, self.y: y_batch})
                 eta = (time.time() - start) * (nb - batch)
                 eta = '%d:%02d' % (eta // 60, eta % 60)
                 print("{}/{} ETA:{}".format(batch, nb, eta))
                 batch += 1
-
             except tf.errors.OutOfRangeError:
                 break
-
-
+        # TODO: check if HH is invertible
         self.sess.run(B_op)
         print("Training of ELM {} ended in {}:{:5f}".format(self.name, ((time.time() - t0) // 60),
-                                                         ((time.time() - t0) % 60)))
+                                                            ((time.time() - t0) % 60)))
+        print("#" * 100)
 
-        print("#"*100)
-
+        # Compute train accuracy
         train_metric = self.evaluate(dataset)
-
         if self.type is 'c':
-
             print('Train accuracy: ', train_metric)
-
-        else: #regression
-
+        else:  # regression
             print('Train MSE: ', train_metric)
 
-
-
-
     def get_Hw_Hb(self):
-
         Hw = self.Hw.eval()  # get hidden layer weights matrix
         Hb = self.Hb.eval()  # get hidden layer biases
-
         return Hw, Hb
 
-
     def get_B(self):
-
         return self.B.eval()
 
-
-
     def get_HH(self):
-
         return self.HH.eval()
 
-
     def __del__(self):
-
         self.sess.close()
         tf.reset_default_graph()
-        self
         print("TensorFlow graph resetted")
-
-
-
-
-
-
-
-
-
-
-
--- TF/prova.py
+++ TF/prova.py
@@ -1,55 +1,43 @@
 import h5py as h5
-
+import keras
 from keras.datasets import mnist
-
-from elm import elm
-
+from TF.elm import elm
 import tensorflow as tf
-
 import os
 
-input_size = 784
-output_size = 10
-n_neurons= 2048
-
-ortho_w = tf.orthogonal_initializer()
-unit_b = tf.uniform_unit_scaling_initializer()
-
-init_w = tf.get_variable(name='init_w',shape=[input_size,n_neurons], initializer=ortho_w)#tf.Variable(initial_value=ortho_w.__call__(shape=[input_size,n_neurons])) # correct way to use initializers
-init_b = tf.Variable(initial_value=unit_b.__call__(shape=[n_neurons]))
-
-
-savedir = os.getcwd() + '/elm_tf_test'
-
-
-elm1 = elm(input_size,output_size,n_neurons, savedir, type='r')
-
-
-hw = elm1.Hw
-
-
-elm2 = elm(input_size,output_size,n_neurons, savedir, w_initializer= init_w, b_initializer=init_b, )
-
-train , test  = mnist.load_data(savedir + "/mnist.txt")
-
+######################################################################################################################
+savedir = os.getcwd() + '/elm_tf_test/'
+# Get dataset
+print('MNIST DATASET')
+train, test = mnist.load_data()
 x_train,y_train = train
 x_test, y_test = test
-
-import keras
-
+del train, test
 y_train = keras.utils.to_categorical(y_train, num_classes=10)
 y_test = keras.utils.to_categorical(y_test, num_classes=10)
-
 x_train = x_train.reshape(-1,28*28)
 x_test = x_test.reshape(-1,28*28)
 
-
 dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
 
+# Hyperparameters
+input_size = x_train.shape[1]
+output_size = 10
+n_neurons = 5000
+batch_size = 5000
+norm = 10 ** -3
 
-elm2.train(dataset,batch_size=2000)
-print(elm2.batch_predict(tf.data.Dataset.from_tensor_slices((x_test, y_test))))
+elm1 = elm(input_size, output_size, n_neurons, savedir, name='default', type='c', l2norm=None, batch_size=batch_size)
+elm1.train(dataset)
+print('Default init: ', elm1.evaluate(tf.data.Dataset.from_tensor_slices((x_test, y_test))))
+elm1.__del__()
 
+dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+ortho_w = tf.orthogonal_initializer()
+unit_b = tf.uniform_unit_scaling_initializer()
+elm2 = elm(input_size, output_size, n_neurons, savedir, name='orth', w_initializer=ortho_w, b_initializer=unit_b,
+           l2norm=None, batch_size=batch_size)
+elm2.train(dataset)
+print('Ortho init: ', elm2.evaluate(tf.data.Dataset.from_tensor_slices((x_test, y_test))))
 
-elm1.train(dataset,batch_size=2000)
-print(elm1.batch_predict(tf.data.Dataset.from_tensor_slices((x_test, y_test))))
+os.system('tensorboard --logdir=%s' % savedir)
